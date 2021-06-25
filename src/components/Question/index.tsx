617a9e98e4c40ce65412e31b0e48789e4c21ff16
Hideki <alexandrejilek@gmail.com>
2021-06-24 23:15:22 -0300

    Quinto commit - dia 5

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import cx from 'classnames'
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -9,16 +11,28 @@ type QuestionProps = {
   };
   children?: ReactNode; //? ReactNode - pode ser qualquer conteúdo .jsx 
   //? (div, outro componente, mais de um componente)
-
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
 export function Question({
   content,
   author,
+  isAnswered = false,
+  isHighlighted = false,
   children,
 }: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      //* alternativa a -> className={`question ${isAnswered ? 'answered' : ''} ${isHighlighted ? 'highlighted' : ''}`}
+      //* é usar a biblioteca 'classNames', precisa apenas instalar usando o "npm add classnames"
+      //* para usar -> cx(nome dado no import) + ( nomesDaClassePadrão + , + { insira "nomeDaClasse" caso o "isAnswered" seja true } )
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
